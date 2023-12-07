fcf709bc29a4e3c4093ad9f609d792956f5770da
Eugen Gribovskij <influras@gmail.com>
2023-12-07 10:54:08 +0100

    paddings

--- lib/src/views/market_view.dart
+++ lib/src/views/market_view.dart
@@ -7,7 +7,6 @@ import 'package:greenglimpse/src/views/product_view.dart';
 // ignore: depend_on_referenced_packages
 import 'package:provider/provider.dart';
 
-
 class MarketView extends StatefulWidget {
   const MarketView({Key? key}) : super(key: key);
 
@@ -32,7 +31,9 @@ class _MarketViewState extends State<MarketView> {
                 // Filter products based on the search query
                 setState(() {
                   filteredProducts = products
-                      .where((product) => product.name.toLowerCase().contains(value.toLowerCase()))
+                      .where((product) => product.name
+                          .toLowerCase()
+                          .contains(value.toLowerCase()))
                       .toList();
                 });
               },
@@ -45,93 +46,68 @@ class _MarketViewState extends State<MarketView> {
           Expanded(
             child: GridView.builder(
               gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200.0, // Maximum width for each grid element
+                maxCrossAxisExtent: 200.0,
                 crossAxisSpacing: 8.0,
                 mainAxisSpacing: 8.0,
-                childAspectRatio: 0.75, // Adjust the aspect ratio as needed
+                childAspectRatio: 0.75,
               ),
-              itemCount: filteredProducts.isEmpty ? products.length : filteredProducts.length,
+              itemCount: filteredProducts.isEmpty
+                  ? products.length
+                  : filteredProducts.length,
               itemBuilder: (context, index) {
-                ProductModel product = filteredProducts.isEmpty ? products[index] : filteredProducts[index];
+                ProductModel product = filteredProducts.isEmpty
+                    ? products[index]
+                    : filteredProducts[index];
+
                 return GestureDetector(
                   onTap: () {
-                    // Handle tapping on the product to navigate to the product description
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => ProductView(product: product)));
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProductView(product: product)),
+                    );
                   },
-                 child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      elevation: 2.0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Image.asset(
-                            product.pictureSource,
-                            height: 120,
-                            fit: BoxFit.cover,
+                  child: Card(
+                    elevation: 2.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Image.asset(
+                          product.pictureSource,
+                          height: 120,
+                          fit: BoxFit.cover,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            product.name,
+                            style: const TextStyle(fontWeight: FontWeight.bold),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              product.name,
-                              style: const TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(ProductController().toCurrencyFormat(product.price)),
-                          ),
-                          // Add a button to add the product to the cart
-                          ElevatedButton(
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(ProductController()
+                              .toCurrencyFormat(product.price)),
+                        ),
+                        // Use Expanded for the button to take equal space
+                        Expanded(
+                          child: ElevatedButton(
                             onPressed: () {
                               // Add the product to the shopping cart
-                              Provider.of<ShoppingCartController>(context, listen: false).addToCart(product);
+                              Provider.of<ShoppingCartController>(context,
+                                      listen: false)
+                                  .addToCart(product);
                             },
                             child: const Text('add to cart'),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 );
               },
             ),
           ),
-        ],
-      ),
-    );
-  }
-
-  // Function to add a product to the shopping cart
-  void addToCart(ProductModel product) {
-    setState(() {
-      shoppingCart.add(product);
-    });
-  }
-
-  // Function to build the shopping cart widget
-  Widget buildShoppingCart() {
-    return Container(
-      margin: const EdgeInsets.all(8.0),
-      padding: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Text(
-            'Shopping Cart',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 8.0),
-          // Display the products in the shopping cart
-          for (ProductModel product in shoppingCart)
-            ListTile(
-              title: Text(product.name),
-              subtitle: Text(ProductController().toCurrencyFormat(product.price)),
-            ),
         ],
       ),
     );
